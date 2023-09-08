aaf5f4ef483d8834c2439370bee38279eaa5843f
VictorNalu <victorchinalurumogu@gmail.com>
2023-09-08 16:56:24 -0700

    2 alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alpahabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('n\n);
+	return (0);
+}
