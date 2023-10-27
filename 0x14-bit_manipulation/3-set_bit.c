055b6acad7ebff678c4eea4f4b1e362c6bb030a6
VictorNalu <victorchinalurumogu@gmail.com>
2023-10-26 22:21:07 -0700

    clear btt

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,16 +4,14 @@
  * set_bit - the given index sets a bit 1
  * @n: it's nothing but a pointer to change a number
  * @index: An index that set a bit 1
- * Return: 1  success, -1 error
+ * Return: 1  success, -1 fail
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int setbit;
-
-if (index > (sizeof(unsigned long int) * 8 - 1))
+if (index > 63)
 return (-1);
-setbit = 1 << index;
-*n = *n | setbit;
+*n = ((1UL << index) | *n);
 return (1);
 }
+
