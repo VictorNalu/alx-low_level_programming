094d79941760a4be1427d411112e71ea0edc2e5d
VictorNalu <victorchinalurumogu@gmail.com>
2023-10-30 11:46:09 -0700

    0-read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (yt == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
+	grit = malloc(sizeof(char) * (letters));
 	if (!grit)
 		return (0);
 
