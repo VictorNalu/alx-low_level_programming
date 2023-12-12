985322be210aac94fb22d4a4593615102c188149
VictorNalu <victorchinalurumogu@gmail.com>
2023-12-12 03:46:25 +0100

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libtest.so
-LD_PRELOAD=$WPD/libtest.so
+wget -P .. https://github.com/VictorNalu/alx-low_level_programming/blob/018b8ee4b7e7a21c76d57599f1ff0607b536d7d2/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD="$WPD/../libtest.so"
